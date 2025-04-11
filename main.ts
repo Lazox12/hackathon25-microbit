788a96226d43aa2394b356e5b0c3a0a5ac913c64
Lazox12 <65539565+Lazox12@users.noreply.github.com>
2025-04-11 21:57:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ enum ServoNum{
     ServoNum4 = 4,
     ServoNum5 = 5
 }
+//% color=190 weight=100 icon="\uf1ec" block="hackathon"
 namespace hackathon {
     class Robot {
         x:number
@@ -22,6 +23,59 @@ namespace hackathon {
         }
         public move(x: number, y: number, z: number) {
             this.x,this.y,this.z = x,y,z;
+            const a = 30 //délka první část
+            const b = 20 //délka druhé části
+            const o = 30 //offset středu soustavy oproti středu robota
+            const p = 10 //výška postavy oproti pracovní ploše
+            const e = 5  //vzálenost POI od posedního kloubu
+            const c = Math.sqrt((x ** 2) + ((o - y) ** 2) + ((z + e - p) ** 2));
+            //console.log(c)
+
+            const r1 = Math.atan(((-1) * x) / (o - y)) / (2 * Math.PI) * 360;
+
+            const r2a = Math.acos(Math.sqrt((x ** 2) + ((o - y) ** 2)) / c);
+            const r2b = Math.acos(((a ** 2) + (c ** 2) - (b ** 2)) / (2 * a * c))
+            //console.log(r2a, r2b)
+            let r2 = 0
+            if ((z + e - p) > 0) {
+                r2 = r2b + r2a;
+            } else {
+                r2 = r2b - r2a;
+            }
+            r2 = r2 / (2 * Math.PI) * 360;
+
+            const r3 = Math.acos(((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b)) / (2 * Math.PI) * 360;
+
+            const r4 = r2 + r3 - 90;
+
+            const r5 = (-1) * r1;
+
+
+            function checkValues():boolean{
+                if (c >= a + b) {
+                    console.log('Bod je mimo dosah!')
+                    return false
+                }
+
+                if (r1 > 90 || r1 < -90) {
+                    console.log('Bod je mimo pracovní plochu!')
+                    return false
+                }
+
+                if (r2 > 90 || r2 < 0 || r3 > 180 || r3 < 0) {
+                    console.log('Neplatné úhly kloubů!')
+                    return false
+                }
+                return true
+            }
+            if(!checkValues()){
+                return
+            }
+            setAngle(0,r1);
+            setAngle(1, r2);
+            setAngle(2, r3);
+            setAngle(3, r4);
+            setAngle(4, r5);
         }
         public setAngle(servo: ServoNum, target: number) {
             let lastAngle: number = this.angles[servo as number];
