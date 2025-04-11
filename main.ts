890c32ef2053d4a081133f7cb75cebd0d924b132
Lazox12 <65539565+Lazox12@users.noreply.github.com>
2025-04-11 18:38:07 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,16 +12,30 @@ namespace hackathon {
         y:number
         z:number
         address:number
+        speedMultiplyer:number
+        delay:number
         angles:number[] = []; 
-        constructor(address = 64) {
+        constructor(address:number = 64,speedMultiplyer:number = 1,delay:number = 100) {
+            this.speedMultiplyer = speedMultiplyer;
+            this.delay = delay;
             this.address = address;
         }
         public move(x: number, y: number, z: number) {
             this.x,this.y,this.z = x,y,z;
         }
-        public setAngle(servo: ServoNum, angle: number) {
-            this.angles[servo as number] = angle;
-            PCA9685.setServoPosition(servo as number, angle, 64);
+        public setAngle(servo: ServoNum, target: number) {
+            let lastAngle: number = this.angles[servo as number];
+            this.angles[servo as number] = target;
+            if(this.speedMultiplyer ==1){
+                PCA9685.setServoPosition(servo as number, target, 64);
+                return;
+            }
+            const itnum:number = (Math.round(1-this.speedMultiplyer)*100+1);
+            let angle:number = (lastAngle-target)/itnum;
+            for(let i:number=0;i<itnum;i++){
+                PCA9685.setServoPosition(servo as number, angle++, 64);
+                basic.pause(this.delay);
+            }
         }
     }
 
@@ -50,5 +64,14 @@ namespace hackathon {
     //% block="get angle|servo: $servo"
     export function getAngle(servo:ServoNum):number{
         return robot.angles[servo as number];
+    }
+    //% block="set i2c address|address: $address"
+    export function setAddress(address:number){
+        robot.address = address;
+    }
+    //% block="set robot speed|speed: $speed"
+    //% v.min=0 v.max=1 v.defl=1
+    export function setSpeed(speed: number) {
+        robot.speedMultiplyer = speed;
     }
 }
