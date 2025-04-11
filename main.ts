3829d8ca5037bc6f3cb8c29ee01bc62d9eaead1a
Lazox12 <65539565+Lazox12@users.noreply.github.com>
2025-04-11 14:18:33 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
+enum ServoNum{
+    ServoNum0 = 0,
+    ServoNum1 = 1,
+    ServoNum2 = 2,
+    ServoNum3 = 3,
+    ServoNum4 = 4,
+    ServoNum5 = 5
+}
 namespace hackathon {
     class Robot {
-
-        constructor() {
-
+        x:number
+        y:number
+        z:number
+        address:number
+        angles:number[] = []; 
+        constructor(address = 64) {
+            this.address = address;
         }
         public move(x: number, y: number, z: number) {
-
+            this.x,this.y,this.z = x,y,z;
         }
-        public setAngle(servo: number, angle: number) {
-
+        public setAngle(servo: ServoNum, angle: number) {
+            this.angles[servo as number] = angle;
+            PCA9685.setServoPosition(servo as number, angle, 64);
         }
     }
 
@@ -22,5 +35,20 @@ namespace hackathon {
     export function setAngle(servo:number,angle:number){
         robot.setAngle(servo,angle);
     }
+    //% block="x"
+    export function x():number{
+        return robot.x;
+    }
+    //% block="y"
+    export function y(): number {
+        return robot.y;
+    }
+    //% block="x"
+    export function z(): number {
+        return robot.z;
+    }
+    //% block="get angle|servo: $servo"
+    export function getAngle(servo:ServoNum):number{
+        return robot.angles[servo as number];
+    }
 }
-
