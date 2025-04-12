6329762500ff4d60e272866571dca776f789edd2
Lazox12 <65539565+Lazox12@users.noreply.github.com>
2025-04-12 02:01:42 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-enum ServoNum{
+enum ServoNum {
     ServoNum0 = 0,
     ServoNum1 = 1,
     ServoNum2 = 2,
@@ -6,23 +6,23 @@ enum ServoNum{
     ServoNum4 = 4,
     ServoNum5 = 5
 }
-//% color=190 weight=100 icon="\uf1ec" block="hackathon"
+//% color=190 weight=100 icon="\uf1ec" block="Robotické Rameno"
 namespace hackathon {
     class Robot {
-        x:number
-        y:number
-        z:number
-        address:number
-        speedMultiplyer:number
-        delay:number
-        angles:number[] = []; 
-        constructor(address:number = 64,speedMultiplyer:number = 1,delay:number = 100) {
+        x: number
+        y: number
+        z: number
+        address: number
+        speedMultiplyer: number
+        delay: number
+        angles: number[] = [];
+        constructor(address: number = 64, speedMultiplyer: number = 1, delay: number = 100) {
             this.speedMultiplyer = speedMultiplyer;
             this.delay = delay;
             this.address = address;
         }
         public move(x: number, y: number, z: number) {
-            this.x,this.y,this.z = x,y,z;
+            this.x, this.y, this.z = x, y, z;
             const a = 30 //délka první část
             const b = 20 //délka druhé části
             const o = 30 //offset středu soustavy oproti středu robota
@@ -51,7 +51,7 @@ namespace hackathon {
             const r5 = (-1) * r1;
 
 
-            function checkValues():boolean{
+            function checkValues(): boolean {
                 if (c >= a + b) {
                     console.log('Bod je mimo dosah!')
                     return false
@@ -68,10 +68,10 @@ namespace hackathon {
                 }
                 return true
             }
-            if(!checkValues()){
+            if (!checkValues()) {
                 return
             }
-            setAngle(0,r1);
+            setAngle(0, r1);
             setAngle(1, r2);
             setAngle(2, r3);
             setAngle(3, r4);
@@ -80,13 +80,13 @@ namespace hackathon {
         public setAngle(servo: ServoNum, target: number) {
             let lastAngle: number = this.angles[servo as number];
             this.angles[servo as number] = target;
-            if(this.speedMultiplyer ==1){
+            if (this.speedMultiplyer == 1) {
                 PCA9685.setServoPosition(servo as number, target, 64);
                 return;
             }
-            const itnum:number = (Math.round(1-this.speedMultiplyer)*100+1);
-            let angle:number = (lastAngle-target)/itnum;
-            for(let i:number=0;i<itnum;i++){
+            const itnum: number = (Math.round(1 - this.speedMultiplyer) * 100 + 1);
+            let angle: number = (lastAngle - target) / itnum;
+            for (let i: number = 0; i < itnum; i++) {
                 PCA9685.setServoPosition(servo as number, angle++, 64);
                 basic.pause(this.delay);
             }
@@ -95,16 +95,16 @@ namespace hackathon {
 
     //% fixedInstance
     export let robot: Robot = new Robot();
-    //% block="move to|x:$x|y:$y|z:$z"
-    export function move(x:number,y:number,z:number) {
-        robot.move(x,y,z);
+    //% block="přemístit se do|x:$x|y:$y|z:$z"
+    export function move(x: number, y: number, z: number) {
+        robot.move(x, y, z);
     }
-    //% block="set servo angle|servo:$servo|angle:$angle"
-    export function setAngle(servo:number,angle:number){
-        robot.setAngle(servo,angle);
+    //% block="nastavit ůhel serva |servo:$servo|angle:$angle"
+    export function setAngle(servo: number, angle: number) {
+        robot.setAngle(servo, angle);
     }
     //% block="x"
-    export function x():number{
+    export function x(): number {
         return robot.x;
     }
     //% block="y"
@@ -115,28 +115,28 @@ namespace hackathon {
     export function z(): number {
         return robot.z;
     }
-    //% block="get angle|servo: $servo"
-    export function getAngle(servo:ServoNum):number{
+    //% block="získat úhel|servo: $servo"
+    export function getAngle(servo: ServoNum): number {
         return robot.angles[servo as number];
     }
-    //% block="set i2c address|address: $address"
-    export function setAddress(address:number){
+    //% block="nastavit i2c adresu|address: $address"
+    export function setAddress(address: number) {
         robot.address = address;
     }
-    //% block="set robot speed|speed: $speed"
+    //% block="nastavit rychlost robota|speed: $speed"
     //% v.min=0 v.max=1 v.defl=1
     export function setSpeed(speed: number) {
         robot.speedMultiplyer = speed;
     }
-    //% block="detect pickup"
-    export function detectPickup():boolean{
-        if(pins.analogReadPin(AnalogPin.P0)<900){
+    //% block="detekce kostky"
+    export function detectPickup(): boolean {
+        if (pins.analogReadPin(AnalogPin.P0) < 900) {
             return true
         }
         return false
     }
-    //% block="detect current"
-    export function detectCurrent(){
+    //% block="detekce chytnutí"
+    export function detectCurrent() {
         return pins.analogReadPin(AnalogPin.P1)
     }
 }
